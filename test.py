6c21919a295b2297dd5419f3dcda2a8b26d909ab
Tracy Xia <tracyxia@berkeley.edu>
2023-05-01 17:47:59 -0700

    miguels updates

--- test.py
+++ test.py
@@ -3,7 +3,9 @@ import pybullet_data
 from dataclasses import dataclass
 import numpy as np
 import time
+import math
 from PIL import Image
+
 
 from quadsim.robots import a1
 
@@ -17,6 +19,19 @@ def createBezier(pts):
 
     return formula
 
+def getBezierTangent(pts):
+    p0 = np.array(pts[0])
+    p1 = np.array(pts[1])
+    p2 = np.array(pts[2])
+    p3 = np.array(pts[3])
+
+    formula = lambda t : 3*(pow(t,2)*(p3-p0+3*p1-3*p2)+2*t*(p0-2*p1+p3)-p0+p1)
+
+    return formula
+
+def moveleg():
+    return
+
 VELOCITY = 1/1500
 ALPHA = 3000
 TOPDOWN = True
@@ -25,13 +40,15 @@ STEP = .25
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
 p.setGravity(0, 0, -10)
 planeId = p.loadURDF('plane.urdf')
 dogStartPos = [0, 0, 0.5]
 dogStartOrientation = p.getQuaternionFromEuler([0, 0, 0])
 dogId = p.loadURDF("model/a1.urdf", dogStartPos, dogStartOrientation)
-targetPos = [2, 2, 1]
+targetPos = dogStartPos
+
 
 #for i in range(21):
     #print(p.getJointInfo(dogId, i))
@@ -56,24 +73,23 @@ bzPoints = [p0, p1, p2, p3]
 
 bezier = createBezier(bzPoints)
 
-
 t = 0
 
 #cam settings
-
-
 
 cyaw=0
 cpitch=-90
 cdist=5
 
-pos, orienation = p.getBasePositionAndOrientation(dogId)
+pos, orientation = p.getBasePositionAndOrientation(dogId)
 
 if(TOPDOWN):
     cpitch = -89
 p.resetDebugVisualizerCamera( cameraDistance=cdist, cameraYaw=cyaw, cameraPitch=cpitch, cameraTargetPosition=pos)
 
 currentPoint = p1Id
+
+closeup = False
 
 while(1):
     keys = p.getKeyboardEvents()
@@ -112,13 +128,20 @@ while(1):
         cdist+=.5
     if keys.get(ord('x')) and keys[ord('x')]&p.KEY_WAS_TRIGGERED:  #X
         cdist-=.5
+    if keys.get(ord('q')) and keys[ord('q')]&p.KEY_WAS_TRIGGERED:
+        closeup = not closeup
 
-    p.resetDebugVisualizerCamera( cameraDistance=cdist, cameraYaw=cyaw, cameraPitch=cpitch, cameraTargetPosition=pos)
+    if closeup:
+        p.resetDebugVisualizerCamera( cameraDistance=.75, cameraYaw=30, cameraPitch=-30, cameraTargetPosition=pos)
+    else:
+        p.resetDebugVisualizerCamera( cameraDistance=cdist, cameraYaw=cyaw, cameraPitch=cpitch, cameraTargetPosition=pos)
     p.resetBasePositionAndOrientation(dogId, pos, dogStartOrientation)
     
-    #if keys.get(ord(' ')): #D (change to space later)
-    if ord(' ') in keys and keys[ord(' ')]&p.KEY_WAS_TRIGGERED:
+    if keys.get(ord('c')) and keys[ord('c')]&p.KEY_WAS_TRIGGERED:
+        p.removeAllUserDebugItems()
         p.resetBasePositionAndOrientation(dogId, dogStartPos, dogStartOrientation)
+        lastPos = dogStartPos
+        targetPos = dogStartPos
 
         p1, _ = p.getBasePositionAndOrientation(p1Id)
         p2, _ = p.getBasePositionAndOrientation(p2Id)
@@ -137,17 +160,75 @@ while(1):
             manhattan += sum(abs(np.subtract(pt1, pt2)))
         ts = 1.0/(manhattan / VELOCITY)
 
+        linecount = 0
         for t in np.arange(0, 1, ts):
-            p.resetDebugVisualizerCamera( cameraDistance=cdist, cameraYaw=30, cameraPitch=-30, cameraTargetPosition=targetPos)
+            if linecount == 3000:
+                p.addUserDebugLine(lastPos,targetPos,[1,0,0])
+                lastPos = targetPos
+                linecount = 0
+            targetPos = bezier(t)
+
+            linecount += 1
+        p.addUserDebugLine(lastPos,targetPos,[1,0,0])
+        
+
+    #--------------------------
+
+    # RUNNING SIMULATION
+
+    #--------------------------
+
+    #if keys.get(ord(' ')): #D (change to space later)
+    if ord(' ') in keys and keys[ord(' ')]&p.KEY_WAS_TRIGGERED:
+        p.removeAllUserDebugItems()
+        p.resetBasePositionAndOrientation(dogId, dogStartPos, dogStartOrientation)
+        lastPos = dogStartPos
+        targetPos = dogStartPos
+
+        p1, _ = p.getBasePositionAndOrientation(p1Id)
+        p2, _ = p.getBasePositionAndOrientation(p2Id)
+        p3, _ = p.getBasePositionAndOrientation(p3Id)
+
+
+        bzPoints = [p0, p1, p2, p3]
+
+        bezier = createBezier(bzPoints)
+        tangent = getBezierTangent(bzPoints)
+
+        manhattan = 0
+        for i in range(3):
+            pt1 = bzPoints[i]
+            pt2 = bzPoints[i+1]
+
+            manhattan += sum(abs(np.subtract(pt1, pt2)))
+        ts = 1.0/(manhattan / VELOCITY)
+
+        linecount = 0
+        for t in np.arange(0, 1, ts):
+            # Draw Path
+            if linecount == 1000:
+                p.addUserDebugLine(lastPos,targetPos,[1,0,0])
+                lastPos = targetPos
+                linecount = 0
+
+            p.resetDebugVisualizerCamera( cameraDistance=.75, cameraYaw=30, cameraPitch=-30, cameraTargetPosition=targetPos)
             p.stepSimulation()
             targetPos = bezier(t)
-            jointindices =    [ 1,   3,   4,   6,   8,   9,  11,  13,  14,  16,  18,  19]
-            targetpositions = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
-            forces =          [50,  50,  50,  50,  50,  50,  50,  50,  50,  50,  50,  50]
-            p.setJointMotorControlArray(dogId,[8, 13],p.POSITION_CONTROL,targetPositions=[0.5, 0.5],forces=[50, 50])
-            #p.setJointMotorControlArray(dogId,[3, 18],p.POSITION_CONTROL,targetPositions=[0.5, 0.5],forces=[1000, 1000])
-            #p.resetBasePositionAndOrientation(dogId, targetPos, dogStartOrientation)
+            heading = tangent(t)
+            print(heading)
+            yaw = math.atan2(heading[1], heading[0])
+            print(yaw)
+            newOrientation = p.getQuaternionFromEuler([0,0,yaw])
 
+            #jointindices =    [ 1,   3,   4,   6,   8,   9,  11,  13,  14,  16,  18,  19]
+            #targetpositions = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
+            #forces =          [50,  50,  50,  50,  50,  50,  50,  50,  50,  50,  50,  50]
+            #p.setJointMotorControlArray(dogId,[8, 13],p.POSITION_CONTROL,targetPositions=[0.5, 0.5],forces=[50, 50])
+            #p.setJointMotorControlArray(dogId,[3, 18],p.POSITION_CONTROL,targetPositions=[0.5, 0.5],forces=[1000, 1000])
+            p.resetBasePositionAndOrientation(dogId, targetPos, newOrientation)
+            linecount += 1
+
+        p.addUserDebugLine(lastPos,targetPos,[1,0,0])
         for t in np.arange(0, 1, .005):
             p.stepSimulation()
     
@@ -158,6 +239,7 @@ while(1):
     #                     forceObj=force, posObj=dogPos, flags=p.WORLD_FRAME)
     
     time.sleep(1./240.)
+    #time.sleep(1./50.)
 
 """
 while(1):
